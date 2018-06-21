9380862513fadc63ece8672a9a6d9eba25f5dbc5
Chirag Gandhi <chiraggandhi18@gmail.com>
2018-06-21 15:25:08 -0500

    Add a user menu

--- CardCatalog/Program.cs
+++ CardCatalog/Program.cs
@@ -8,7 +8,34 @@ namespace CardCatalog
         {
             //I'm so lost man, how does Git work?
             //I know
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter the name of a file: ");
+            bool displaymenu = true;
+
+            if (displaymenu)
+            {
+                MainMenu();
+
+            }
+
+
         }
+
+        private static bool MainMenu()
+        {
+            Console.WriteLine("Enter the name of a file: ");
+            string filename = Console.ReadLine();
+            Console.WriteLine("Select a number");
+            Console.Write("1. List All books");
+            Console.Write("2. Add a book");
+            Console.Write("3. Save and Exit");
+
+            string input = Console.ReadLine();
+
+
+
+            return false;
+        }
+
+
     }
 }
